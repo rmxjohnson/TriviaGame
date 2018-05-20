3261e5a44b082054113c344d81c0eac9db9e261a
rmxjohnson <rhondaxjohnson@gmail.com>
2018-05-20 00:13:55 -0400

    working copy - added question object

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,53 +5,73 @@
 $(document).ready(function () {
     // initialize variables
 
-    // enable the "checkAnswer" to run
+    // enable the "checkAnswer" function to run on-click
     var enabled = true;
 
-    // array of trivia questions
-    var questions = [
-        'On November 17, 1981, approximately 30 million Americans turned on their televisions to watch which couple get married?',
-        'Which of these is not the nickname of one of the ghosts featured in the Pac-Man video game?',
-        'Imelda Marcos, who in 1986 was forced into exile with her husband, Philippine President Ferdinand Marcos, was widely mocked for her collection of a rumored 7,500 what?',
-        'Which of these songs from Michael Jackson\'s legendary 1982 "Thriller" album was not released as a single?',
-        'According to "A Guidebook to All That Is Truly Masculine" the subtitle of a book published in 1982, real men don\'t eat what?',
-        'In a July 1988 concert in East Berlin, this musician told the crowd, in German: "I\'ve come to play rock \'n\' roll for you in the hope that one day all the barriers will be torn down."',
-        'What \'80s sitcom largely took place inside the governor\'s mansion in an unidentified U.S. state?',
-        'Cabbage Patch Kids, among the most sought-after toys of all time, were originally called what?',
-        'Which of these movies did not win the Academy Award for Best Picture in the \'80s?',
-        'President Ronald Reagan gave out jars of which candy to White House visitors?'
-    ];
+    // Constructor function for question object
+    // Properties: question, array of 4 answers, correct answer, image to display
+    var questionObject = function (questionArg, answersArg, correctArg, imageArg) {
+        this.question = questionArg;
+        this.answerArray = answersArg;
+        this.correctAnswer = correctArg;
+        this.image = imageArg;
+    };
 
-    // array of images for the trivia answers
-    var imageArray = [
-        'assets/images/question1LukeLaura.jpg',
-        'assets/images/question2PacMan.png',
-        'assets/images/question3Shoes.jpg',
-        'assets/images/question4MichaelJackson.jpg',
-        'assets/images/question5Quiche.jpg',
-        'assets/images/question6Bruce.jpg',
-        'assets/images/question7Benson.jpg',
-        'assets/images/question8CabbagePatch.jpg',
-        'assets/images/question9ColorPurple.jpg',
-        'assets/images/question10JellyBellys.jpg',
-        'assets/images/question11KristinShepard.jpg',
-        'assets/images/Totally80s.jpg'];
+    // Array of question objects
+    var questionArray = [
+        new questionObject('On November 17, 1981, approximately 30 million Americans turned on their televisions to watch which couple get married?',
+            ['Prince Andrew & Sarah Ferguson', 'Prince Charles & Diana Spencer', 'Princess Anne & Mark Phillips', 'Luke Spencer & Laura Webber'],
+            'Luke Spencer & Laura Webber',
+            'assets/images/question1LukeLaura.jpg'),
+        new questionObject('Which of these is not the nickname of one of the ghosts featured in the Pac-Man video game?',
+            ['Tinky', 'Blinky', 'Inky', 'Pinky'],
+            'Tinky',
+            'assets/images/question2PacMan.png'),
+        new questionObject('Imelda Marcos, who in 1986 was forced into exile with her husband, Philippine President Ferdinand Marcos, was widely mocked for her collection of a rumored 7,500 what?',
+            ['Teddy Bears', 'Handbags', 'Pairs of Shoes', 'Velvet Paintings'],
+            'Pairs of Shoes',
+            'assets/images/question3Shoes.jpg'),
+        new questionObject('Which of these songs from Michael Jackson\'s legendary 1982 "Thriller" album was not released as a single?',
+            ['P.Y.T. (Pretty Young Thing)', 'Baby Be Mine', 'Human Nature', 'The Girl Is Mine'],
+            'Baby Be Mine',
+            'assets/images/question4MichaelJackson.jpg'),
+        new questionObject('According to "A Guidebook to All That Is Truly Masculine" the subtitle of a book published in 1982, real men don\'t eat what?',
+            ['Tofu', 'Sushi', 'Quiche', 'Canapes'],
+            'Quiche',
+            'assets/images/question5Quiche.jpg'),
+        new questionObject('In a July 1988 concert in East Berlin, this musician told the crowd, in German: "I\'ve come to play rock \'n\' roll for you in the hope that one day all the barriers will be torn down."',
+            ['Elton John', 'Bruce Springsteen', 'Billy Joel', 'Michael Jackson'],
+            'Bruce Springsteen',
+            'assets/images/question6Bruce.jpg'),
+        new questionObject('What \'80s sitcom largely took place inside the governor\'s mansion in an unidentified U.S. state?',
+            ['Mr. Belvedere', 'Webster', 'Benson', 'Silver Spoons'],
+            'Benson',
+            'assets/images/question7Benson.jpg'),
+        new questionObject('Cabbage Patch Kids, among the most sought-after toys of all time, were originally called what?',
+            ['Little People', 'Backyard Babies', 'Turnip Tots', 'Young \'Uns'],
+            'Little People',
+            'assets/images/question8CabbagePatch.jpg'),
+        new questionObject('Which of these movies did not win the Academy Award for Best Picture in the \'80s?',
+            ['Driving Miss Daisy', 'Rain Man', 'Chariots of Fire', 'The Color Purple'],
+            'The Color Purple',
+            'assets/images/question9ColorPurple.jpg'),
+        new questionObject('President Ronald Reagan gave out jars of which candy to White House visitors?',
+            ['Jelly Bellys', 'Hershey Kisses', 'M&Ms', 'Gummi Bears'],
+            'Jelly Bellys',
+            'assets/images/question10JellyBellys.jpg')
+    ]
 
-    //  key for the correct answer choices
-    correctAnswer = ["4", "1", "3", "2", "3", "2", "3", "1", "4", "1"];
+    var currentObject;
 
-    // array of answers
-    answerOne = ['Prince Andrew & Sarah Ferguson', 'Tinky', 'Teddy Bears', 'P.Y.T. (Pretty Young Thing)', 'Tofu', 'Elton John', 'Mr. Belvedere', 'Little People', 'Driving Miss Daisy', 'Jelly Bellys'];
-    answerTwo = ['Prince Charles & Diana Spencer', 'Blinky', 'Handbags', 'Baby Be Mine', 'Sushi', 'Bruce Springsteen', 'Webster', 'Backyard Babies', 'Rain Man', 'Hershey Kisses'];
-    answerThree = ['Princess Anne & Mark Phillips', 'Inky', 'Pairs of Shoes', 'Human Nature', 'Quiche', 'Billy Joel', 'Benson', 'Turnip Tots', 'Chariots of Fire', 'M&Ms'];
-    answerFour = ['Luke Spencer & Laura Webber', 'Pinky', 'Velvet Paintings', 'The Girl Is Mine', 'Canapes', 'Michael Jackson', 'Silver Spoons', 'Young \'Uns', 'The Color Purple', 'Gummi Bears'];
-
+    // image to display on stats page
+    var lastImage = 'assets/images/Totally80s.jpg';
+   
     var questionCounter = 0;                    // counter for question #
     var correctCount = 0;                       // # of correct answers
     var incorrectCount = 0;                     // # of incorrect answers
     var unansweredCount = 0;                    // # of unanswered questions
     var maxTime = 10;                           // # of seconds the player has to answer the question
-    var delayTime = 5000                        // 5000 milliseconds = 5 seconds
+    var delayTime = 4000                        // 4000 milliseconds = 4 seconds
     var timeLeft;                               // seconds left on the imter
     var timerInterval;                          // interval for the timer
 
@@ -71,8 +91,9 @@ $(document).ready(function () {
     var imageContainer = $("#imageContainer");
     var currentImage = $("#currentImage");
 
-
-
+    //*************************
+    //  Function Definitions  *
+    //*************************
 
     // Execute when game is done
     var gameOver = function () {
@@ -82,18 +103,12 @@ $(document).ready(function () {
         // display start button
         startButton.css("visibility", "visible");
 
-        // reset counters for a new game
-        questionCounter = 0;
-        correctCount = 0;
-        incorrectCount = 0;
-        unansweredCount = 0;
-
         // display game stats
         firstChoice.text("***    Thanks for Playing    ***");
         secondChoice.text("Correct Answers: " + correctCount);
         thirdChoice.text("Incorrect Answers: " + incorrectCount);
         fourthChoice.text("Unanswered Questions: " + unansweredCount);
-        currentImage.attr("src", imageArray[(imageArray.length) - 1]);
+        currentImage.attr("src", lastImage);
         imageContainer.removeClass("hideItem");
         startButton.text("Restart");
         timerDisplay.empty();
@@ -101,6 +116,12 @@ $(document).ready(function () {
         currentMessage.empty();
         extraMessage.css("visibility", "visible");
         extraMessage.text("GAME OVER - Press Restart to Play Again");
+
+        // reset counters for a new game
+        questionCounter = 0;
+        correctCount = 0;
+        incorrectCount = 0;
+        unansweredCount = 0;
     }
 
     // execute when need a new question
@@ -111,6 +132,7 @@ $(document).ready(function () {
         // set variables
         timeLeft = maxTime;
         var i = questionCounter;
+
 
         // allow questions to display
         answerSection.removeClass("hideItem");
@@ -123,23 +145,24 @@ $(document).ready(function () {
         timerDisplay.text("Timer: ");
         showTimer.text(timeLeft);
 
-        // Game Over when used all questions
-        if (questionCounter == questions.length) {
+        // Game Over when all questions have been displayed
+        if (questionCounter == questionArray.length) {
             gameOver();
-
         }
         else {
             // begin the timer
             beginTimer();
 
+            currentObject = questionArray[i];
+          
             // display question and choices
-            currentMessage.text(questions[i]);
+            currentMessage.text(currentObject.question);
             extraMessage.text("placeholder");
             extraMessage.css("visibility", "hidden");
-            firstChoice.text(answerOne[i]);
-            secondChoice.text(answerTwo[i]);
-            thirdChoice.text(answerThree[i]);
-            fourthChoice.text(answerFour[i]);
+            firstChoice.text(currentObject.answerArray[0]);
+            secondChoice.text(currentObject.answerArray[1]);
+            thirdChoice.text(currentObject.answerArray[2]);
+            fourthChoice.text(currentObject.answerArray[3]);
         }
     }
 
@@ -151,7 +174,7 @@ $(document).ready(function () {
 
     // execute when no time left
     function outOfTime() {
-        extraMessage.text("OOPS! You Ran Out of Time!");
+        extraMessage.text("OOPS!  TIME'S UP!");
         extraMessage.css("visibility", "visible");
 
         displayCorrectAnswer();
@@ -159,13 +182,13 @@ $(document).ready(function () {
         // add 1 to count of unanswered questions
         unansweredCount++;
 
-        // increment the question counter
+        // increment the question # counter
         questionCounter++;
 
         // stop the timer 
         stopTimer();
 
-        // after 5 seconds, display a new question
+        // after a delay, display a new question
         setTimeout(newQuestion, delayTime);
     }
 
@@ -187,27 +210,11 @@ $(document).ready(function () {
 
     function displayCorrectAnswer() {
         // Display the answer's image
-        currentImage.attr("src", imageArray[questionCounter]);
-        imageContainer.removeClass("hideItem");
+        imageContainer.removeClass('hideItem');
+        currentImage.attr("src", currentObject.image);
 
-        // find which answer is correct and display the correct answer
-        var temp = correctAnswer[questionCounter];
-
-        if (temp == 1) {
-            firstChoice.text(answerOne[questionCounter]);
-        }
-        else if (temp == 2) {
-            firstChoice.text(answerTwo[questionCounter]);
-        }
-        else if (temp == 3) {
-            firstChoice.text(answerThree[questionCounter]);
-        }
-        else if (temp == 4) {
-            firstChoice.text(answerFour[questionCounter]);
-        }
-        else {
-            firstChoice.text("Error Handler - Programmed incorrectly");
-        }
+        // display the correct answer
+        firstChoice.text('Correct Answer:   ' + currentObject.correctAnswer);
 
         // clear other text
         secondChoice.empty();
@@ -225,7 +232,7 @@ $(document).ready(function () {
             enabled = false;
 
             // answer chosen by the player
-            var playerPick = $(this).attr("value");
+            var playerPick = $(this).text();
 
             // stop the timer
             stopTimer();
@@ -234,7 +241,7 @@ $(document).ready(function () {
             displayCorrectAnswer();
 
             // player chose correct answer
-            if (playerPick == correctAnswer[questionCounter]) {
+            if (playerPick == currentObject.correctAnswer) {
                 // display message to the player
                 extraMessage.text("***  FANTABULOUS - You Got This!!!  ***");
                 extraMessage.css("visibility", "visible");
@@ -258,15 +265,14 @@ $(document).ready(function () {
 
             // after 5 seconds, display a new question
             setTimeout(newQuestion, delayTime);
-
         }
-
     };
 
-    //*********************************
-    //    Begin new game when player clicks Start/Restart button
+    //*************************************************************
+    //    Begin new game when player clicks Start/Restart button  *
+    //*************************************************************
     startButton.click(newQuestion);
 
-    // when player clicks on selection, check the answer
+    // when player clicks on an answer, check the answer
     answerSection.on("click", "h4", checkAnswer);
 })
